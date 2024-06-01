826afa717bc0d3909d5b1a1b0a0c248ad9aeb533
Osipov Evgeniy Vasilyevich <evgeosipov@ozon.ru>
2024-06-01 21:41:34 +0700

    Check to setup for github

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV ? '/homepage/' : '/',
   plugins: [
     vue(),
     vueJsx(),
